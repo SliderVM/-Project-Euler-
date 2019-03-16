4da35a867639984175b9e6daa1ced1a7875ab622
Михаил Юрьевич <slidersoft@gmail.com>
2019-03-16 15:22:47 +0500

    Задача 16

---
+++ Power digit sum (16)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+using System.Text;
+
+namespace Power_digit_sum__16_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new BigInteger();
+            number = 2;
+            for(var i=0; i<999; i++)
+            {
+                number *= 2;
+            }
+
+            var strNumber = number.ToString();
+            int res = 0;
+            foreach(var i in strNumber)
+            {
+                res += (int)char.GetNumericValue(i);
+            }
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
